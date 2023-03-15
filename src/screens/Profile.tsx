e7ed767ba92e51f5d4a754345368a42ded67f6c7
ssilman <ssilman@gmail.com>
2023-03-15 20:20:52 -0300

    complements

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -3,12 +3,62 @@ import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
-import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
-import { TouchableOpacity } from 'react-native';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from 'native-base';
+import { Alert, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 const PHOTO_SIZE = 33;
 
 export function Profile() {
+
+  const toast = useToast();
+
+  const [userPhoto, setUserPhoto] = useState('https://github.com/sillmann.png');
+
+  async function handleUserPhotoSelect(){
+
+    setPhotoIsLoading(true);
+    
+    try {
+    
+    const photoSelected = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+      aspect: [4,4],
+      allowsEditing: true
+    });
+
+    if (photoSelected.canceled) {
+      return;
+    }
+
+    if (photoSelected.uri) {
+      const photoInfo = await FileSystem.getInfoAsync(photoSelected.uri);
+
+      if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+        return toast.show({
+        title: "Essa imagem é muito grande. Escolha uma de até 5MB",
+        placement: 'top',
+        bgColor: 'red.500'
+        })
+      }
+
+      // return Alert.alert("Essa imagem é muito grande. Escolha uma de até 5MB");
+
+      // if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+      //   return Alert.alert("Essa imagem é muito grande. Escolha uma de até 5MB");
+      // }
+      setUserPhoto(photoSelected.assets[0].uri);
+    }
+
+
+    } catch (error) {
+        console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
 
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
 
@@ -35,14 +85,16 @@ export function Profile() {
             />
             :
             <UserPhoto
-            source={{ uri: 'https://github.com/sillmann.png'}}
+            source={{ uri: userPhoto}}
             alt=""
             size={PHOTO_SIZE}
             /> 
 
           }
 
-          <TouchableOpacity>
+          <TouchableOpacity 
+            onPress={handleUserPhotoSelect}
+          >
             <Text
               color="green.500"
               fontWeight="bold"
