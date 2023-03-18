0d94befec2289e5496a28bc60fd33767a53062f3
ssilman <ssilman@gmail.com>
2023-03-17 21:09:43 -0300

    complements

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,6 +1,9 @@
+import { api } from '@services/api';
+
 import { useState } from 'react';
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base';
 
 import LogoSvr from '@assets/logo.svg';
 
@@ -8,6 +11,11 @@ import BackgroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+
+import axios from 'axios';
+import { AppError } from '@utils/AppError';
+
+
 
 type FormDataProps = {
   name: string;
@@ -17,6 +25,8 @@ type FormDataProps = {
 }
 
 export function SignUp(){
+
+  const toast = useToast();
 
   const [name, setName ] = useState('');
   const [email, setEmail ] = useState('');
@@ -30,19 +40,42 @@ export function SignUp(){
   }
 
   async function handleSignUp() {
-    const response = await fetch('http://192.168.0.110:3333/users', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email, password })
-    });
-    // .then(response => response.json())
-    // .then(data => console.log(data))
 
-    const data = await Response.json();
-    console.log(data);
+    try{
+      const response = await api.post('/users',{ name, email, password });
+      console.log(response.data);
+    } catch (error) {
+        const isAppError = error instanceof AppError;
+        const title = isAppError ? error.message : 'Não foi possível criar a conta'
+        
+        toast.show({
+          title,  
+          placement: 'top',
+          bgColor: 'red.500'
+        });
+
+      // if (axios.isAxiosError(error)){
+      //    Alert.alert(error.response?.data.message);
+      //   // Alert.alert(title);
+      // }
+    }
+
+    // const response = await api.post('/users',{ name, email, password });
+    // console.log(response.data);
+
+    // const response = await fetch('http://192.168.0.110:3333/users', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ name, email, password })
+    // });
+    // // .then(response => response.json())
+    // // .then(data => console.log(data))
+
+    // const data = await Response.json();
+    // console.log(data);
   }
 
   return (
