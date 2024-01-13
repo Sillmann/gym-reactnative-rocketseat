e04b2d66352d509b8a2ff52e1cac83781cb13654
ssilman <ssilman@gmail.com>
2024-01-12 22:56:06 -0300

    atualizacoes

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -37,7 +37,15 @@ export function SignUp() {
   }
 
   function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-    console.log({ name, email, password, password_confirm })
+    //console.log({ name, email, password, password_confirm })
+    fetch('http://192.168.0.110:3333/users', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password })
+    })
   }
 
   return (
